f43d4f0483e05231277c51f35078e883beb12bf6
Developer Ashish <therobomarket@gmail.com>
2022-07-21 12:12:17 +0530

    stage 01 base implimentation

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -14,25 +14,22 @@ logging.basicConfig(filename = os.path.join(log_dir, "running_logs.log"),
                                             filemode = 'a')   
                          
         
-def get_data(config_path):
+def get_data(config_path, params_path):
     config = read_yaml(config_path)
-    source_download_dirs = config["source_download_dirs"]
-    local_data_dirs = config["local_data_dirs"]
+    params = read_yaml(params_path)
     
-    for source_download_dir, local_data_dir in tqdm(zip(source_download_dirs, local_data_dirs), total = 2, desc = "List of Folders", colour = 'red'):
-        create_directory(local_data_dirs)
-        copy_file(source_download_dir, local_data_dir)
         
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default = "config/config.yaml")
+    args.add_argument("--params", "-p", default = "params.yaml")
     parsed_args = args.parse_args()
     
     try:
         logging.info(">>>>> Stage one started")
-        get_data(config_path = parsed_args.config)
-        logging.info("Stage one completed all the data are saved in local >>>>>")
+        get_data(config_path = parsed_args.config, params_path = parsed_args.params)
+        logging.info("Stage one completed!!! >>>>>\n")
     except Exception as e:
         logging.exception(e)
         raise e
